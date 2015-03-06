7f2ed2eabae2b25866f68f4dc004ecb1cb9803ce
myzaki <mougel@intechinfo.fr>
2015-03-06 15:54:41 +0100

    first commit 2

---
+++ TestJenkins/TestAjoutClasse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCIJenkins
+{
+    public class TestAjoutClasse
+    {
+        string message;
+        public string GetMessage()
+        {
+            Message = "hello";
+            return Message;
+        }
+
+        public string Message
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestCIJenkins;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestAjoutClasse test = new TestAjoutClasse();
+            Assert.Equals(test.GetMessage(),"hello");
+        }
+    }
+}
